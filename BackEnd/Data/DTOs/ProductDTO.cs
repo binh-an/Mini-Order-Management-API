3d6fdfe16594fb3ab2dc0083873ce275eee3660d
Nguyễn Thị Bình An <nguyenbinhan2707@gmail.com>
2025-11-18 23:20:02 +0700

    add ProductDTO

---
+++ BackEnd/Data/DTOs/ProductDTO.cs
@@ -0,0 +1,25 @@
+namespace Data.DTOs
+{
+    public class ProductDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public int StockQuantity { get; set; }
+    }
+    public class CreateProductDTO
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public int StockQuantity { get; set; }
+    }
+    public class UpdateProductDTO
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public int StockQuantity { get; set; }
+    }
+}
