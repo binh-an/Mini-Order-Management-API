42bebb8ba46d5b87aa67b5296e2c71a39a18cf39
Hoàng Hạc Hiếu <hachieu375@gmail.com>
2025-11-19 12:09:45 +0700

    Setup complete DTO structure and AutoMapper for Order & OrderDetail

--- BackEnd/Program.cs
+++ BackEnd/Program.cs
@@ -3,6 +3,9 @@ using Data.Entities;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddAutoMapper(typeof(MappingProfile));
+
+
 // 1. Add DbContext with SQL Server
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
