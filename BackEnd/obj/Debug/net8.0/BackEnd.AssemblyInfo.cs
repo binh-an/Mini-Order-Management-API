7f7caee0b9469dab3c4e626c1ef759805c479771
Hoàng Hạc Hiếu <hachieu375@gmail.com>
2025-11-19 10:09:42 +0700

    ok

--- BackEnd/obj/Debug/net8.0/BackEnd.AssemblyInfo.cs
+++ BackEnd/obj/Debug/net8.0/BackEnd.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BackEnd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42510d3d9dd27e0da42be9ec1f110e0014730f33")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("BackEnd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BackEnd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
