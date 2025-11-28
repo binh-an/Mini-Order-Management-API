702ce7f8d3bd88564e022d85d08ace4609f85f74
Nguyễn Thị Bình An <nguyenbinhan2707@gmail.com>
2025-11-29 01:16:08 +0700

    IIS

--- BackEnd/obj/Debug/net8.0/BackEnd.AssemblyInfo.cs
+++ BackEnd/obj/Debug/net8.0/BackEnd.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BackEnd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ec7c7d2cc312532f2f6e031d75fac067fd4f28a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba7ae1e6db8189ed1ca75fefb57fab14828efa52")]
 [assembly: System.Reflection.AssemblyProductAttribute("BackEnd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BackEnd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
