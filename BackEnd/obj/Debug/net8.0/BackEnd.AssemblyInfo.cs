803f7d5b8c9f373e51e9135d575b30027ba0dc91
Hồ Cẩm Tú <tuho6418@gmail.com>
2025-11-29 10:02:55 +0700

    add quantity

--- BackEnd/obj/Debug/net8.0/BackEnd.AssemblyInfo.cs
+++ BackEnd/obj/Debug/net8.0/BackEnd.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BackEnd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a23b21418a22923e453003c8c903972bf51d598")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19c94681405b942dc3696407bba8748c8efb6a47")]
 [assembly: System.Reflection.AssemblyProductAttribute("BackEnd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BackEnd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
